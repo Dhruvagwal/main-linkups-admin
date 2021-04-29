9ed73b55746133a581d417f4de363365325747f3
Dhruvagwal <dhruv@clus.in>
2021-04-29 16:27:14 +0530

    UI_Commit

---
+++ src/screen/order/orderDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const orderDetail = () => {
+    return (
+        <View>
+            <Text>Order Detail</Text>
+        </View>
+    )
+}
+
+export default orderDetail
+
+const styles = StyleSheet.create({})
